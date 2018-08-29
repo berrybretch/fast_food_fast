ee02b97630ac2d55acce1729e4e9ae5167fee6b2
berrybretch <berrybretch@gmail.com>
2018-08-30 02:03:55 +0300

    Modified UI in index.html Added functions for cart Added css for index, cart

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,23 @@
-$(document).ready(
-	
+$(document).ready(function(){
+
+	$('img').on('dragstart', function(event){
+		event.preventDefault();
+	})
+
+	$('.add').click(function(){		
+		x = parseInt($('.cart_amount').text())
+		x = x + 1
+		$('.cart_amount').text(x) 
+	})
+
+	$('.remove').click(function(){		
+		x = parseInt($('.cart_amount').text()) 
+		if (x == 0) {
+			$('.cart_amount').text(0)
+		}else{
+			$('.cart_amount').text(x-1)
+		}
+	})
 
 
 
@@ -10,4 +28,6 @@ $(document).ready(
 
 
 
-	)
+
+
+})
