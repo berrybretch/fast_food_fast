8fb01560bcf513004ca92068f0784d8242a9900d
berrybretch <berrybretch@gmail.com>
2018-08-30 23:59:36 +0300

    Added admin dash page Added css and js for Cart buttons Added links to sign_in page

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,28 @@
 $(document).ready(function(){
+	
 
 	$('img').on('dragstart', function(event){
 		event.preventDefault();
 	})
-
-	$('.add').click(function(){		
-		x = parseInt($('.cart_amount').text())
-		x = x + 1
-		$('.cart_amount').text(x) 
+	$('.addqty').click(function(){
+		x = parseInt($(this).next('h3').text())
+		x = x+1
+		$(this).next('h3').text(x)
 	})
 
-	$('.remove').click(function(){		
-		x = parseInt($('.cart_amount').text()) 
+	$('.subqty').click(function(){
+		x = parseInt($(this).prev('h3').text())
 		if (x == 0) {
-			$('.cart_amount').text(0)
+			$(this).prev('h3').text(x)
 		}else{
-			$('.cart_amount').text(x-1)
+			$(this).prev('h3').text(x-1)		
 		}
 	})
 
 
 
 
-
-
-
-
-
-
-
-
+	
 })
+
+
